b89ced263fab18886f2fd303c584e1b7c9bf4a59
saeed Ahmadian <38537212+saeedahmadian@users.noreply.github.com>
2019-10-27 22:06:24 -0400

    Add files via upload

--- DDPG.py
+++ DDPG.py
@@ -1,12 +1,4 @@
-"""
-Deep Deterministic Policy Gradient (DDPG), Reinforcement Learning.
-DDPG is Actor Critic based algorithm.
-Pendulum example.
-View more on my tutorial page: https://morvanzhou.github.io/tutorials/
-Using:
-tensorflow 1.0
-gym 0.8.0
-"""
+
 
 import tensorflow as tf
 import numpy as np
